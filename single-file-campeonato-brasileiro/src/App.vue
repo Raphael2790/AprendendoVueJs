48a57c3b6eb050ed282cea31a541ed376c515895
Raphael Silvestre dos Santos <raphael.silvestre@animaeducacao.com.br>
2021-01-06 16:57:01 -0300

    Mudando a aplicação para single file components

---
+++ single-file-campeonato-brasileiro/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+        <titulo></titulo>
+        <div class="row">
+            <div class="col-md-12">
+                <!--$event para pegar o evento passado pela intância Vue da camada inferior -->
+                <novo-jogo></novo-jogo>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-md-12" v-show="visao == 'tabela'">
+                <tabela-clubes></tabela-clubes>
+                <total-gols/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+   data () {
+        return {
+        visao: 'tabela',
+        timeCasa: null,
+        timeFora: null
+}
+    },
+    methods: {
+        mostrarTabela(event) {
+            this.visao = event
+        },
+        mostrarPlacar({visao, timeCasa, timeFora}) {
+            this.visao = visao;
+            this.timeCasa = timeCasa;
+            this.timeFora = timeFora;
+        }
+    },
+    filters: {
+        saldo(time) {
+            return time.gm - time.gs; 
+        }
+    }
+}
+</script>
---
+++ single-file-campeonato-brasileiro/src/components/Titulo.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="row">
+        <!-- $parent- podemos pegar qualquer propriedade e métodos do elemento pai, podemos também subir níveis usando encadeamento $parent.$parent -->
+        <h1>{{$parent.visao | ucWords}} Campeonato Brasileiro 2020 - Série A</h1>
+    </div>
+</template>
